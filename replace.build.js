dc0a2d5025ce3cbc53f314914e68708b4e73c39c
spotless\Hacker-pc <gkkomensi@gmail.com>
2017-10-11 17:38:34 +0300

    introduced deployed app versioning

--- replace.build.js
+++ replace.build.js
@@ -1,16 +1,16 @@
 var replace = require('replace-in-file');
-var package = require("./package.json");
-var buildVersion = package.version;
+var package1 = require("./package.json");
+var buildVersion = package1.version;
 const options = {
-  files: 'src/environments/environment.prod.ts',
-  replace: /version: '(.*)'/g,
-  with: "version: '"+ buildVersion + "'",
-  allowEmptyPaths: false,
+  files: './src/environments/environment.prod.ts',
+  from: /version: '(.*)'/g,
+  to: "version: '"+ buildVersion + "'",
+  allowEmptyPaths: false
 };
 
 try {
   var changedFiles = replace.sync(options);
-  if (changedFiles == 0) {
+  if (changedFiles === 0) {
     throw "Please make sure that file '" + options.files + "' has \"version: ''\"";
   }
   console.log('Build version set: ' + buildVersion);
