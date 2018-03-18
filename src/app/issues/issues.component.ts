f19b49c56413e0f97989129dd2548abd54def29a
George Komen <g.komen@carepay.co.ke>
2018-03-18 23:08:25 +0300

    styling issue page

--- src/app/issues/issues.component.ts
+++ src/app/issues/issues.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SunamiserviceService } from '../sunamiservice.service';
-import { paymentRatesClass, paymentRatesClassPerClient } from '../classes/paymentRates';
 import { ToasterService, Toast } from 'angular2-toaster';
-import { CompleterService, CompleterData } from 'ng2-completer';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 import { UserServiceService } from '../user-service.service';
 
@@ -13,15 +10,15 @@ import { UserServiceService } from '../user-service.service';
   styleUrls: ['./issues.component.css']
 })
 export class IssuesComponent implements OnInit {
-   customer_id: string = "";
-   issuearray: any[];
-   data: any[];
-   filterQuery = "";
-   rowsOnPage = 100;
-   sortOrder = "asc";
-   issuesolvecomment: string = "";
-   solveissuediv: boolean = false;
-   Id: Number = 0;
+  customer_id: string = "";
+  issuearray: any[];
+  data: any[] = [];
+  filterQuery = "";
+  rowsOnPage = 100;
+  sortOrder = "asc";
+  issuesolvecomment: string = "";
+  solveissuediv: boolean = false;
+  Id: Number = 0;
 
   constructor(private _SunamiService: SunamiserviceService, private toasterService: ToasterService) {
 
@@ -32,30 +29,27 @@ export class IssuesComponent implements OnInit {
     this.getIssues();
   }
 
-   getIssues() {
-    this.data = null;
+  getIssues() {
+    this.data = [];
     this._SunamiService.getIssues().subscribe(
-      (data) => this.data = data, //Bind to view
+      (data) => this.data = data,
       err => {
-        // Log errors if any
         this.popToast("no internet", err, this.data);
       });
   }
 
-   solveIssue(s1: number) {
+  solveIssue(s1: number) {
     this.Id = s1;
-    //show div
     this.solveissuediv = true;
   }
 
-   submitissuesolve() {
+  submitissuesolve() {
     this.issuearray = [];
-    if (this.Id > 0 && this.issuesolvecomment.length > 4) {
+    if (this.Id > 0 && this.issuesolvecomment.length > 2) {
       this.issuearray.push({ Id: this.Id, Ssolver: UserServiceService.email, Scomment: this.issuesolvecomment });
       this._SunamiService.postSolveIssues(this.issuearray).subscribe(
         (data) => this.popToastpost("results", data), //Bind to view
         err => {
-          // Log errors if any
           this.popToast("no internet", err, this.data);
         });
     }
@@ -66,25 +60,24 @@ export class IssuesComponent implements OnInit {
     this.issuesolvecomment = "";
   }
 
-   cancelsubmitissuesolve() {
+  cancelsubmitissuesolve() {
     //clear other fields
     this.issuesolvecomment = "";
     this.solveissuediv = false;
   }
 
-   popToastpost(t: string, d: any[]) {
+  popToastpost(t: string, d: any[]) {
     var toast: Toast = {
       type: 'error',
       title: t,
       body: d
     };
     this.toasterService.pop(toast);
-    //fetch new data afta registration
     this.getIssues();
   }
 
 
-   popToast(t: string, b: string, d: any[]) {
+  popToast(t: string, b: string, d: any[]) {
     this.data = d;
     var toast: Toast = {
       type: 'error',
@@ -94,7 +87,7 @@ export class IssuesComponent implements OnInit {
     this.toasterService.pop(toast);
   }
 
-   hideloader() {
+  hideloader() {
     document.getElementById("loading").style.display = "none";
   }
 
