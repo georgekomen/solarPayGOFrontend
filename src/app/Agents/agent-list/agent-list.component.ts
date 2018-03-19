f8e46b7d6539368a5f6fb97c70295620185558f4
George Komen <g.komen@carepay.co.ke>
2018-03-19 07:50:16 +0300

    list and register agents

--- src/app/Agents/agent-list/agent-list.component.ts
+++ src/app/Agents/agent-list/agent-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {SunamiserviceService} from "../../sunamiservice.service";
+import {Toast, ToasterService} from "angular2-toaster";
+import {UserServiceService} from "../../user-service.service";
+import {GeneralFilterPipe} from "../../general-filter.pipe";
+import {AgentPayload} from "../shared/AgentPayload";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-agent-list',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgentListComponent implements OnInit {
 
-  constructor() { }
+  public data: AgentPayload[] = [];
+  public filterQuery = "";
+  public rowsOnPage = 100;
+  public sortOrder = "asc";
 
-  ngOnInit() {
+  constructor(private router: Router, private _SunamiService: SunamiserviceService, private toasterService: ToasterService,private userservice: UserServiceService) {
+
   }
 
+  ngOnInit(): void {
+    this.getAgents();
+  }
+
+  registerNewAgent(){
+    this.router.navigate(['agentregister']);
+  }
+
+  getAgents() {
+    this._SunamiService.getAgents().subscribe(
+      (data) => {
+        this.data = data;
+        this.hideloader();
+      },
+      err => {
+        this.popToast("no internet", err);
+      });
+  }
+
+  public popToast(t: string, b: string) {
+    var toast: Toast = {
+      type: 'error',
+      title: t,
+      body: b
+    };
+    this.toasterService.pop(toast);
+  }
+
+  hideloader() {
+    document.getElementById("loading").style.display = "none";
+  }
+
+  exporttoexcel() {
+    this.userservice.exporttoexcel(GeneralFilterPipe.filteredArray, "test1");
+  }
 }
--- src/app/Agents/agent-registration/agent-registration.component.ts
+++ src/app/Agents/agent-registration/agent-registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AgentPayload} from "../shared/AgentPayload";
+import {Toast, ToasterService} from "angular2-toaster";
+import {SunamiserviceService} from "../../sunamiservice.service";
+import {UserServiceService} from "../../user-service.service";
 
 @Component({
   selector: 'app-agent-registration',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgentRegistrationComponent implements OnInit {
 
-  constructor() { }
+  agent:AgentPayload = new AgentPayload();
+  constructor(private _SunamiService: SunamiserviceService, private toasterService: ToasterService) {
+
+  }
 
   ngOnInit() {
+
+  }
+
+  submit() {
+    if (this.agent.idnumber != null && this.agent.idnumber != "") {
+      this._SunamiService.registerAgent(this.agent).subscribe(
+        (data) => this.popToast("results", data),
+        err => {
+          this.popToast("no internet", err);
+        });
+    } else {
+      this.popToast("error", "Fill all fields appropriately");
+    }
+  }
+
+  popToast(t: string, b: string) {
+    var toast: Toast = {
+      type: 'error',
+      title: t,
+      body: b
+    };
+    this.toasterService.pop(toast);
   }
 
 }
