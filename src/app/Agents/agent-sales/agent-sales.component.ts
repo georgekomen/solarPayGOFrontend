dba3e6a4f9233b96d5fb79b44cd844d4852111f3
George Komen <g.komen@carepay.co.ke>
2018-03-19 10:05:44 +0300

    can see agent sales

--- src/app/Agents/agent-sales/agent-sales.component.ts
+++ src/app/Agents/agent-sales/agent-sales.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import {AgentSalePayload} from "../shared/AgentSalePayload";
+import {SunamiserviceService} from "../../sunamiservice.service";
 
 @Component({
   selector: 'app-agent-sales',
@@ -8,14 +10,30 @@ import {ActivatedRoute, Params} from "@angular/router";
 })
 export class AgentSalesComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  public filterQuery = "";
+  public rowsOnPage = 100;
+  public sortOrder = "asc";
+  data: AgentSalePayload[] = [];
+  constructor(private activatedRoute: ActivatedRoute, private _SunamiService: SunamiserviceService) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params: Params)=>{
       const agentcode =  params['agentcode'];
+      this.getAgentSales(agentcode);
     },error2 => {
 
     });
+  }
+
+  getAgentSales(id) {
+    this._SunamiService.getAgentSales(id).subscribe(res => {
+      this.data = res;
+      this.hideloader();
+    });
+  }
+
+  hideloader() {
+    document.getElementById("loading").style.display = "none";
   }
 
 }
